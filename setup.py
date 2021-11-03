83c0e06131ca9d7fb161b375e5612bd7d3bd6a52
Tushar Iyer <tushariyer@users.noreply.github.com>
2021-11-03 12:27:45 -0400

    Packaging action patch
    Attempting to patch the packaging action

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='canvas',
+      version='0.1.1',
+      py_modules=['canvas'],
+      description='Canvas | The Project Base',
+      author='Tushar Iyer',
+      author_email='redacted@email.site',
+      url='https://github.com/tushariyer/canvas',
+      )
