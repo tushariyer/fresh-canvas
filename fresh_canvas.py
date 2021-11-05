693156f09c098e84bbb5b99ae0ba830bf8ae154c
Tushar Iyer <tushariyer@users.noreply.github.com>
2021-11-05 14:19:41 -0400

    Minor Annotations
    Minor Annotations

--- fresh_canvas.py
+++ fresh_canvas.py
@@ -7,6 +7,7 @@ import canvas_utilities as cutils
 
 
 def new(name, is_github):
+    # Kickoff
     print(f'Creating directories for {name}')
     os.mkdir(name)
     cutils.save_base_gitignore(name)
@@ -14,35 +15,43 @@ def new(name, is_github):
     path_list = directories.paths
     created_sample_test = False
 
+    # Check if the project should have basic GitHub Actions created for it
     if is_github == "n":
         path_list = directories.remove_github_path()
+
+    # Create all the base paths
     for path in path_list:
         os.makedirs(path)
 
     created_directories = cutils.get_all_subdirectories(project_root_dir)
-
     cutils.create("main")
 
     print('Initializing Python directories')
     for directory in created_directories:
+        # Add basic GitHub Actions
         if directory.endswith("workflows"):
             print('Adding Base GitHub Actions')
 
+        # Don't add __init__.py files anywhere we don't need them
         if any(x not in directory for x in directories.dont_init_as_py):
             os.chdir(directory)
             open("__init__.py", "w")
 
+            # Add a sample unit test so the users have a starting point
             if directory.endswith("Test"):
                 print('Creating sample Python test')
                 cutils.create_sample_test()
                 created_sample_test = True
             elif directory.endswith("API"):
-                cutils.create("controller")
+                cutils.create("controller")  # Basic API Controller file
             elif directory.endswith("Read"):
                 cutils.create("read_data")
             elif directory.endswith("Write"):
                 cutils.create("write_data")
 
+            # Go back to the root. Theoretically if we were able to
+            # create a test sample then we should be able to run
+            # tests and assert that python stuff is good to go
             os.chdir(project_root_dir)
 
     if created_sample_test:
@@ -51,6 +60,7 @@ def new(name, is_github):
 
 
 if __name__ == '__main__':
+    # User Prompts
     project_name = input("Enter Project Name:\n")
     is_github_project = input("Is this a GitHub hosted solution? (y/n)\n")
 
